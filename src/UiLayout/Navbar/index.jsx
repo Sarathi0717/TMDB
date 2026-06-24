6ae92005dabb8ed52dc6eb559aedfd1446b7204e
Sarathi0717 <madheshsarathi3@gmail.com>
2026-06-24 19:15:22 +0530

    Fix debounce

--- src/UiLayout/Navbar/index.jsx
+++ src/UiLayout/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import useDebounce from "../../Hooks/usedebounce";
+import useDebounce from "../../Hooks/useDebounce";
 import "./index.scss";
 
 const Navbar = ({ onSearch }) => {
